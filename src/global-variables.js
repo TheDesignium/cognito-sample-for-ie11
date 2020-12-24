c76cfb48dbffa98baeefc8ece9b7e71c1d84dd98
emrum01 <emrum01@gmail.com>
2020-12-25 01:51:52 +0900

    This is the sample

--- src/global-variables.js
+++ src/global-variables.js
@@ -1,7 +1,7 @@
-export const REGION = 'us-east-2_x2Lx7LLXr';
+export const REGION = 'input_region';
 export const POOL_DATA = {
-  UserPoolId: 'us-east-2_x2Lx7LLXr',
-  ClientId: '4dikqv0a9st8i9rojt491qou9f'
+  UserPoolId: 'input_user_id_here',
+  ClientId: 'input_client_id_here'
 };
-export const IDENTITY_POOL_ID = 'us-east-2:68810ba6-a1c5-49d4-8aa9-5e24e3ca0b03';
+export const IDENTITY_POOL_ID = 'input_identity_pool_id';
 export const LOGINS_KEY = 'cognito-idp.' + REGION + '.amazonaws.com/' + POOL_DATA.UserPoolId;
